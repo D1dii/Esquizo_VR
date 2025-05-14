5145c2caa42d934ebf138f798b09f71812fa12ea
DavidLastra8 <david.lastra@estudiantat.upc.edu>
2025-05-14 12:54:02 +0200

    la linterna agarra, pero no se si se enciende o no tbh

--- EsquizoVR/Assets/LanternManager.cs
+++ EsquizoVR/Assets/LanternManager.cs
@@ -1,41 +1,73 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR;
 using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine.XR.Interaction.Toolkit.Interactables;
+using UnityEngine.XR.Interaction.Toolkit.Interactors;
 
 public class LanternManager : MonoBehaviour
 {
-    public class Linterna : MonoBehaviour
+    [Header("Flashlight Settings")]
+    public Light flashlight;
+    public XRBaseInteractor leftHandInteractor;
+
+    private XRGrabInteractable grabInteractable;
+    [SerializeField, Tooltip("Estado actual de la linterna (solo lectura)")]
+    private bool isOn = false;
+
+    private bool isHeldByLeftHand = false;
+    private InputDevice leftHandDevice;
+    private bool buttonPressedLastFrame = false;
+
+    private void Awake()
     {
-        public Light spotLight;  
-        public XRController controller; 
+        grabInteractable = GetComponent<XRGrabInteractable>();
+    }
 
-        private bool isOn = true;
+    private void OnEnable()
+    {
+        grabInteractable.selectEntered.AddListener(OnSelectEntered);
+        grabInteractable.selectExited.AddListener(OnSelectExited);
+    }
 
-        void Start()
+    private void OnDisable()
+    {
+        grabInteractable.selectEntered.RemoveListener(OnSelectEntered);
+        grabInteractable.selectExited.RemoveListener(OnSelectExited);
+    }
+
+    private void OnSelectEntered(SelectEnterEventArgs args)
+    {
+        isHeldByLeftHand = args.interactorObject.transform == leftHandInteractor.transform;
+
+        if (isHeldByLeftHand)
         {
-            if (spotLight == null)
-            {
-                Debug.LogError("No se ha asignado la luz de la linterna.");
-            }
+            
+            InputDevices.GetDeviceAtXRNode(XRNode.LeftHand).TryGetFeatureValue(CommonUsages.primaryButton, out _);
+            leftHandDevice = InputDevices.GetDeviceAtXRNode(XRNode.LeftHand);
         }
+    }
 
-        void Update()
-        {
-      
-            if (controller)
-            {
-                if (controller.selectInteractionState.activatedThisFrame)
-                {
-                    ToggleLight();
-                }
-            }
-        }
+    private void OnSelectExited(SelectExitEventArgs args)
+    {
+        isHeldByLeftHand = false;
+    }
 
-        void ToggleLight()
+    private void Update()
+    {
+        if (!isHeldByLeftHand) return;
+
+        
+        if (leftHandDevice.isValid && leftHandDevice.TryGetFeatureValue(CommonUsages.primaryButton, out bool buttonPressed) && buttonPressed)
         {
-            isOn = !isOn;
-            spotLight.enabled = isOn;
+            ToggleFlashlight();
         }
-    } 
+    }
+
+    private void ToggleFlashlight()
+    {
+        isOn = !isOn;
+        flashlight.enabled = isOn;
+    }
 }
