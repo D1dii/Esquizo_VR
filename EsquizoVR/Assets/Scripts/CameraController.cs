a99697dc55d26ac3e6c242a1e9e2ae629241c394
D1dii <didacgp123@gmail.com>
2025-05-14 18:54:25 +0200

    Input Done

--- EsquizoVR/Assets/Scripts/CameraController.cs
+++ EsquizoVR/Assets/Scripts/CameraController.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.XR.Interaction.Toolkit;
 using UnityEngine.XR.Interaction.Toolkit.Interactables;
 
 public class CameraController : MonoBehaviour
@@ -8,6 +10,20 @@ public class CameraController : MonoBehaviour
     public Transform playerTransform;
 
     public XRGrabInteractable interactable;
+
+    public InputActionReference cameraShotAction;
+
+    private void Awake()
+    {
+        cameraShotAction.action.Enable();
+        cameraShotAction.action.performed += ctx => TakeCameraShot();
+    }
+
+    private void OnDestroy()
+    {
+        cameraShotAction.action.performed -= ctx => TakeCameraShot();
+        cameraShotAction.action.Disable();
+    }
 
     // Start is called before the first frame update
     public void Start()
@@ -23,10 +39,20 @@ public class CameraController : MonoBehaviour
         {
             FollowCharacterNotGrabbed();
         }
+
+        
     }
 
     public void FollowCharacterNotGrabbed()
     {
         transform.position = new Vector3(playerTransform.position.x + 0.5f, transform.position.y, playerTransform.position.z - 0.5f);
+    }
+
+    public void TakeCameraShot()
+    {
+        if (interactable.isSelected)
+        {
+            Debug.Log("Camera Shot Taken!");
+        }
     }
 }
