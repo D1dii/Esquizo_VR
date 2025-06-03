1c927b26ac982241d65efbfa9e82aae1e7dc8cd5
Alex Becerra Garcia <abecerragarcia04@gmail.com>
2025-06-04 00:57:21 +0200

    Shadows y espejo

--- EsquizoVR/Assets/MirrorAnomally.cs
+++ EsquizoVR/Assets/MirrorAnomally.cs
@@ -6,9 +6,21 @@ public class MirrorAnomally : Anomaly
 {
     public bool playerHasEntered = false;
     public bool alreadyPlayed = false;
+    public Material mirrorMaterial;
 
     public override void InitAnomaly()
     {
         Destroy(LevelManager.instance.AnomaliesAndModel["Mirror"]);
     }
+
+    private void Update()
+    {
+        // Aquí podrías agregar lógica adicional si es necesario
+        if (alreadyPlayed == true)
+        {
+            // cambiar el material
+            mirrorMaterial.color = Color.red; // Cambia el color del material a rojo
+        }
+    }
 }
+
